5387245ef17777770867863787688de0e468922a
Walt Sorensen <photodude@users.noreply.github.com>
2019-03-27 22:57:44 -0600

    Add some modifications from the dual-g2-high-power-motor-shield library
    These changes better mirror the dual-g2-high-power-motor-shield library example
    https://github.com/pololu/dual-g2-high-power-motor-shield

--- examples/Demo/Demo.ino
+++ examples/Demo/Demo.ino
@@ -8,56 +8,65 @@ void stopIfFault()
 {
   if (md.getFault())
   {
-    Serial.println("Motor fault");
+	md.Sleep(); // put the driver to sleep on fault
+	delay(1);
+	Serial.println("Motor fault");
     while(1);
   }
 }
 
 void setup()
 {
-  // start serial port
-  Serial.begin(115200);
-  Serial.println("G2 Motor Driver");
+	// start serial port
+	Serial.begin(115200);
+	Serial.println("G2 Motor Driver");
 
-  // Start up the library
-  md.init();
+	// Start up the library
+	md.init();
+	md.calibrateCurrentOffsets();
+	delay(10);
 }
 
 void loop()
 {
-  for (int i = 0; i <= 400; i++)
-  {
-    md.setSpeed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("Motor current: ");
-      Serial.println(md.getCurrentReading());
-    }
-    delay(2);
-  }
-  
-  for (int i = 400; i >= -400; i--)
-  {
-    md.setSpeed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("Motor current: ");
-      Serial.println(md.getCurrentReading());
-    }
-    delay(2);
-  }
-  
-  for (int i = -400; i <= 0; i++)
-  {
-    md.setSpeed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("Motor current: ");
-      Serial.println(md.getCurrentReading());
-    }
-    delay(2);
-    }
+	md.Wake(); // Wake up the motor driver
+
+	for (int i = 0; i <= 400; i++)
+	{
+		md.setSpeed(i);
+		stopIfFault();
+		if (i%200 == 100)
+		{
+			Serial.print("Motor current: ");
+			Serial.println(md.getCurrentReading());
+		}
+		delay(2);
+	}
+
+	for (int i = 400; i >= -400; i--)
+	{
+		md.setSpeed(i);
+		stopIfFault();
+		if (i%200 == 100)
+		{
+			Serial.print("Motor current: ");
+			Serial.println(md.getCurrentReading());
+		}
+		delay(2);
+	}
+
+	for (int i = -400; i <= 0; i++)
+	{
+		md.setSpeed(i);
+		stopIfFault();
+		if (i%200 == 100)
+		{
+			Serial.print("Motor current: ");
+			Serial.println(md.getCurrentReading());
+		}
+		delay(2);
+	}
+
+	md.Sleep(); // Put the Motor driver into sleep mode.
+	delay(500);
 }
